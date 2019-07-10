b0ecbb43be39910a485f17c012cf551b5bacd1b3
takidog <wow@taki.dog>
2019-07-10 17:50:26 +0800

    Add api login route

--- src/utils/util.py
+++ src/utils/util.py
@@ -1,6 +1,20 @@
 import string
 import random
+import falcon
 
 
 def randStr(lens):
     return ''.join([random.choice(string.ascii_letters + string.digits) for n in range(lens)])
+
+
+def max_body(limit):
+    def hook(req, resp, resource, params):
+        length = req.content_length
+        if length is not None and length > limit:
+            msg = ('The size of the request is too large. The body must not '
+                   'exceed ' + str(limit) + ' bytes in length.')
+
+            raise falcon.HTTPPayloadTooLarge(
+                'Request body is too large', msg)
+
+    return hook
