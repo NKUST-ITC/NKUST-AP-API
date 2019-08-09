681ec4e70dd766eb452c02208d748b293e16c070
takidog <wow@taki.dog>
2019-08-09 23:28:25 +0800

    Add webap login check decorator

--- src/utils/util.py
+++ src/utils/util.py
@@ -1,6 +1,8 @@
 import string
 import random
 import falcon
+from cache.ap_cache import login as webap_login
+from utils import error_code
 
 
 def randStr(lens):
@@ -18,3 +20,37 @@ def max_body(limit):
                 'Request body is too large', msg)
 
     return hook
+
+
+def webap_login_cache_required(req, resp, resource, params):
+    """This function is for falcon.before to use, like a decorator,
+    check user have cache cookie.
+
+    Args:
+        req ([type]): falcon default.
+        resp ([type]): falcon default.
+        resource ([type]): falcon default.
+        params ([type]): falcon default.
+
+    Raises:
+        falcon.HTTPUnauthorized: HTTP_401,Just login fail,or maybe NKUST server is down.
+        falcon.HTTPServiceUnavailable: HTTP_503, NKUST server problem.
+        falcon.HTTPInternalServerError: HTTP_500, something error.
+
+    Returns:
+        [bool]: True, login success.
+    """
+    # jwt payload
+    payload = req.context['user']['user']
+    login_status = webap_login(
+        username=payload['username'], password=payload['password'])
+
+    if login_status == error_code.CACHE_WENAP_LOGIN_SUCCESS:
+        return True
+    elif login_status == error_code.CACHE_WEBAP_LOGIN_FAIL:
+        raise falcon.HTTPUnauthorized(description='login fail')
+    elif login_status == error_code.CACHE_WEBAP_SERVER_ERROR:
+        raise falcon.HTTPServiceUnavailable()
+    elif login_status == error_code.CACHE_WEBAP_ERROR:
+        raise falcon.HTTPInternalServerError()
+    raise falcon.HTTPInternalServerError()
