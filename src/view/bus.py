dea0bd00a6dc64a6aec88570dd43b842bdc10aa8
takidog <wow@taki.dog>
2019-08-25 02:08:14 +0800

    Fix variable name of typo

--- src/view/bus.py
+++ src/view/bus.py
@@ -13,23 +13,23 @@ class busUserReservations:
     def on_get(self, req, resp):
 
         payload = req.context['user']['user']
-        reslut = bus_cache.bus_reservations_record(
+        result = bus_cache.bus_reservations_record(
             username=payload['username'])
 
-        if isinstance(reslut, str):
-            resp.body = reslut
+        if isinstance(result, str):
+            resp.body = result
             resp.media = falcon.MEDIA_JSON
             resp.status = falcon.HTTP_200
             return True
         # error handle
-        elif isinstance(reslut, int):
-            if reslut == error_code.CACHE_BUS_COOKIE_ERROR:
+        elif isinstance(result, int):
+            if result == error_code.CACHE_BUS_COOKIE_ERROR:
                 raise falcon.HTTPInternalServerError(
                     description='api server not found cookie.')
-            elif reslut == error_code.CACHE_BUS_USER_ERROR:
+            elif result == error_code.CACHE_BUS_USER_ERROR:
                 raise falcon.HTTPForbidden(
                     description='NKUST server not found user data.')
-            elif reslut == error_code.BUS_TIMEOUT_ERROR:
+            elif result == error_code.BUS_TIMEOUT_ERROR:
                 raise falcon.HTTPServiceUnavailable(description='timeout')
 
         raise falcon.HTTPInternalServerError(
