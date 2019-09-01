ceb297ede1dcf22149886c11e9b6ee6c70e8d4ff
takidog <wow@taki.dog>
2019-09-01 23:15:11 +0800

    Add basic news system

---
+++ src/news/news.py
@@ -0,0 +1,173 @@
+
+import datetime
+import json
+
+import redis
+
+from utils import config
+
+red_news = redis.StrictRedis.from_url(
+    url=config.REDIS_URL, db=8, charset="utf-8", decode_responses=True)
+
+
+def get_news(all_news, news_id):
+    """get a news.
+
+    Args:
+        all_news ([list]): must be sorted, get this data from _get_all_news.
+        news_id ([int]): news id
+
+    Returns:
+        [dict]: news data
+        [bool]: False
+        [None]: not found news.
+    """
+    if len(all_news) < 1:
+        return None
+    temp_news_index = -1
+    news_next = {}
+    news_last = {}
+
+    for index, value in enumerate(all_news):
+        if value['id'] == news_id:
+            temp_news_index = index
+            try:
+                news_next = all_news[index+1]
+            except:
+                news_next = {}
+            try:
+                news_last = all_news[index-1]
+                if index-1 < 0:
+                    news_last = {}
+            except:
+                news_last = {}
+
+    if temp_news_index < 0:
+        # not found news
+        return None
+
+    # mix news next id and news last id to dict
+    all_news[temp_news_index]['nextId'] = news_next.get('id', None)
+    all_news[temp_news_index]['lastId'] = news_last.get('id', None)
+
+    return all_news[temp_news_index]
+
+
+def get_all_news():
+    # public
+    all_news_data = _get_all_news()
+    return [get_news(all_news_data, i['id']) for i in all_news_data]
+
+
+def _get_all_news():
+    # private
+    news = sorted([json.loads(red_news.get(i))
+                   for i in red_news.scan_iter()], key=lambda k: k['id'])
+    return news
+
+
+def time_format(time_str):
+    """Change world all timezone to utc.
+
+    Args:
+        time_str ([str]): '2019-08-31T06:33:29Z'
+                          '2019-08-31T06:33:29H'
+                          or more ...
+        https://en.wikipedia.org/wiki/List_of_military_time_zones
+
+    Returns:
+        [datetime.datetime]: utc datetime 
+        [bool]: False error format.
+    """
+    try:
+        negative = -1
+        time_zone = {
+            "A": 1,
+            "B": 2,
+            "C": 3,
+            "D": 4,
+            'E': 5,
+            'F': 6,
+            'G': 7,
+            'H': 8,
+            'I': 9,
+            'K': 10,
+            'L': 11,
+            'M': 12,
+            'N': 1*negative,
+            'O': 2*negative,
+            'P': 3*negative,
+            'Q': 4*negative,
+            'R': 5*negative,
+            'S': 6*negative,
+            'T': 7*negative,
+            'U': 8*negative,
+            'V': 9*negative,
+            'W': 10*negative,
+            'X': 11*negative,
+            'Y': 12*negative,
+            'Z': 0
+        }[time_str[-1]]
+    except:
+        return False
+    raw_time = datetime.datetime.strptime(time_str[:-1], "%Y-%m-%dT%H:%M:%S")
+    utc_time = raw_time + datetime.timedelta(hours=-time_zone)
+    return utc_time
+
+
+def add_news(**kwargs):
+    """Add news to redis.
+
+    Kwargs:
+        title   [str]:     Required.
+        img_url [str]:     Optional.
+        url     [str]:     Link, optional.
+        description [str]: Optional.
+        expireTime  [str]: ISO 8601, must have timezone at last character.
+
+            2019-09-2T11:33:29H
+            2019-09-2T11:33:29Z
+            2019-09-2T11:33:29A
+            ...
+
+    Returns:
+        [bool]: False
+        [int]: Success, return news id.
+    """
+    title = kwargs.get('title', False)
+    if not title:
+        return False
+
+    news_name = "news_{news_id}"
+    id_count = len([i for i in red_news.scan_iter()])
+    news_id = 0
+    for id_ in range(id_count+1):
+        if not red_news.exists(news_name.format(news_id=id_)):
+            news_id = id_
+            break
+
+    news_data = {
+        "title": title,
+        "id": news_id,
+        "publishedAt": datetime.datetime.utcnow().isoformat(timespec="seconds")+"Z",
+        "imgUrl": kwargs.get('img_url', None),
+        "url": kwargs.get('url', None),
+        "description": kwargs.get('description', None)
+    }
+    expire_time_seconds = kwargs.get('expireTime', None)
+    if kwargs.get('expireTime', False):
+        utc = time_format(kwargs.get('expireTime', False))
+        expire_time_seconds = (utc-datetime.datetime.utcnow()).seconds
+    data_dumps = json.dumps(news_data, ensure_ascii=False)
+
+    red_news.set(name=news_name.format(news_id=news_id),
+                 value=data_dumps, ex=expire_time_seconds)
+    return True
+
+
+def update_news(news_id):
+    pass
+
+
+def remove_news(news_id):
+    pass
