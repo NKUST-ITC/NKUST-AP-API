94381df0a2cd8a4bd9a2cfc1e4a65aa5c428d845
takidog <wow@taki.dog>
2019-09-25 10:25:03 +0800

    Add leave submit crawler

--- src/crawler/leave_crawler.py
+++ src/crawler/leave_crawler.py
@@ -187,3 +187,91 @@ def get_submit_info(session):
     result['timeCode'] = [i.text for i in time_code[3:]]
 
     return result
+
+
+def leaves_submut(session, leaves_data, proof_file=None, proof_file_name="test.jpg", proof_type='jpg'):
+
+    main_url = 'http://leave.nkust.edu.tw/CK001MainM.aspx'
+    req = session.get(main_url)
+    root = etree.HTML(req.text)
+
+    form_data = {i.attrib["name"]: i.attrib["value"] for i in root.xpath(
+        "//input") if i.attrib["name"] != "ctl00$ButtonLogOut"}
+
+    req = session.post(url=main_url, data=form_data)
+    root = etree.HTML(req.text)
+
+    form_data = {i.attrib.get("name"): i.attrib.get("value", "") for i in root.xpath("//input") if i.attrib["name"] not in [
+        "ctl00$ButtonLogOut", 'ctl00$ContentPlaceHolder1$CK001$ButtonQuery', 'ctl00$ContentPlaceHolder1$CK001$ButtonClear', 'ctl00$ContentPlaceHolder1$CK001$ButtonPreview']}
+
+    start_date = leaves_data['days'][0]['day']
+    end_date = leaves_data['days'][-1]['day']
+    form_data['ctl00$ContentPlaceHolder1$CK001$DateUCCBegin$text1'] = start_date
+    form_data['ctl00$ContentPlaceHolder1$CK001$DateUCCEnd$text1'] = end_date
+    req = session.post(url=main_url, data=form_data)
+    root = etree.HTML(req.text)
+    try:
+        if root.xpath("//script[@language='javascript']")[0].text.find('alert') >= 0:
+            return error_code.LEAVE_SUBMIT_WRONG_DATE  # 日期不對//或是不再學期中
+    except:
+        pass
+
+    global_form_data = {}
+    global_form_data['ctl00$ContentPlaceHolder1$CK001$TextBoxReason'] = leaves_data['reasonText']
+    global_form_data['ctl00$ContentPlaceHolder1$CK001$ddlTeach'] = leaves_data['teacherId']
+    global_form_data['ctl00$ContentPlaceHolder1$CK001$RadioButtonListOption'] = leaves_data['leaveType']
+    time_code = root.xpath(
+        "//div[@id='ContentPlaceHolder1_CK001_UpdatePanel2']//tr")
+
+    need_click_button_data = []
+    # get need click button information
+    for leave_day in leaves_data['days']:
+        for tr in time_code:
+            _temp = tr.xpath('.//td')
+            for index, value in enumerate(_temp[3:]):
+                if leave_day['day'] == _temp[1].text:
+                    if len(value.text) >= 40:
+                        if index in leave_day['class']:
+                            need_click_button_data.append(
+                                value.xpath('.//input')[0].attrib.get("name", False))
+    # set leave button data
+    for click_data in need_click_button_data:
+        form_data = {i.attrib.get("name"): i.attrib.get("value", "") for i in root.xpath(
+            "//input") if i.attrib["name"][0:2] == "__"}
+        form_data.update(global_form_data)
+        form_data[click_data] = ""
+        req = session.post(url=main_url, data=form_data)
+        root = etree.HTML(req.text)
+
+    # next step!
+    form_data = {i.attrib.get("name"): i.attrib.get("value", "") for i in root.xpath(
+        "//input") if i.attrib["name"][0:2] == "__"}
+    form_data.update(global_form_data)
+    form_data['ctl00$ContentPlaceHolder1$CK001$ButtonCommit2'] = '下一步'
+    req = session.post(url=main_url, data=form_data)
+    root = etree.HTML(req.text)
+
+    # delete defalut headers
+    del session.headers['Content-Type']
+
+    form_data = {i.attrib.get("name"): i.attrib.get("value", "") for i in root.xpath(
+        "//input") if i.attrib["name"][0:2] == "__"}
+    form_data['ctl00$ContentPlaceHolder1$CK001$ButtonSend'] = '存檔'
+
+    # change to multipart request
+    if proof_file != None:
+        files = {'ctl00$ContentPlaceHolder1$CK001$FileUpload1': (
+            proof_file_name, proof_file, 'image/%s' % proof_type)}
+    else:
+        files = {'ctl00$ContentPlaceHolder1$CK001$FileUpload1': (None, '')}
+
+    req = session.post(url=main_url, data=form_data, files=files)
+    root = etree.HTML(req.text)
+
+    alert = root.xpath("//script[@language='javascript']")[0].text
+    if alert.find('成功') >= 0:
+        return error_code.LEAVE_SUBMIT_SUCCESS
+    elif alert.find('假單請檢附附檔') >= 0:
+        return error_code.LEAVE_SUBMIT_NEED_PROOF
+    else:
+        return error_code.LEAVE_SUBMIT_DATE_CONFLICT  # 重複請假
